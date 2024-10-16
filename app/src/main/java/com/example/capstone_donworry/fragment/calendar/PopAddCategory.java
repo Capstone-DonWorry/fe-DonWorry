87374565c8bbee04116590637677358d27912fe9
wonjinSong <wjsong0204@naver.com>
2024-10-16 19:42:42 +0900

    ✨ Feat:항목 추가 기능 설정
    날짜 입력 받기, 카테고리 설정 페이지 이동

--- app/src/main/java/com/example/capstone_donworry/fragment/calendar/PopAddCategory.java
+++ app/src/main/java/com/example/capstone_donworry/fragment/calendar/PopAddCategory.java
@@ -1,2 +1,72 @@
-package com.example.capstone_donworry.fragment.calendar;public class PopAddCategory {
+package com.example.capstone_donworry.fragment.calendar;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.capstone_donworry.R;
+
+public class PopAddCategory extends DialogFragment {
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Dialog의 윈도우 설정
+        Dialog dialog = getDialog();
+        if (dialog != null) {
+            Window window = dialog.getWindow();
+            if (window != null) {
+                // 디바이스의 화면 크기를 가져오기 위한 DisplayMetrics 객체 생성
+                DisplayMetrics displayMetrics = new DisplayMetrics();
+                requireActivity().getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+                // 화면 크기의 90%를 계산
+                int width = (int) (displayMetrics.widthPixels * 0.9);
+
+                // Dialog의 크기 설정
+                WindowManager.LayoutParams layoutParams = window.getAttributes();
+                layoutParams.width = width;
+                window.setAttributes(layoutParams);
+            }
+        }
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+        if (dialog.getWindow() != null) {
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        }
+        return dialog;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        // 팝업창 레이아웃 사용
+        View view = inflater.inflate(R.layout.pop_add_category, container, false);
+        if (view == null) {
+            Log.d("PopAddCategory", "View inflation failed");
+        } else {
+            Log.d("PopAddCategory", "View inflated successfully");
+
+        }
+
+        return view;
+    }
 }
